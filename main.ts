e154618a25d12586c16f52eafb5e441760bb8f73
JordanDesjardins1 <153002008+JordanDesjardins1@users.noreply.github.com>
2023-12-19 11:25:57 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,5 @@
+let base1 = game.askForNumber("Enter the value for base1")
+let base2 = game.askForNumber("Enter the value for base2")
+let height = game.askForNumber("Enter the value for height")
+let area = (base1 + base2) / 2 * height
+game.splash("Area is " + area)
